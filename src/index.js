e1bf0f5a2de08cbbb22d2bfc88e022c63cbf324f
Sai Prasad Dash <sabitadash007@gmail.com>
2020-11-13 02:08:05 +0530

    Added send message and location functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
+const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +15,36 @@ const publicDirectoryPath = path.join(__dirname,'../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port,()=>{
+// let count = 0 ;
+
+io.on('connection',(socket)=>{
+    console.log('New web socket connection');
+
+    socket.emit('message','Welcome!');
+
+    socket.broadcast.emit('message','A new user has joined!');
+
+    socket.on('sendMessage',(message,callback)=>{
+        const filter = new Filter();
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed');
+        }
+
+        io.emit('message',message);
+        callback();
+    });
+
+    socket.on('sendLocation',({latitude,longitude},callback)=>{
+        io.emit('message',`https://google.com/maps?q=${latitude},${longitude}`);
+        callback();
+    });
+
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left');
+    });
+});
+
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}!`);
 });
